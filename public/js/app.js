c2294c58c1fdfa6c9dd9615a25c91b53cb91d46c
samf98 <samir.facusse@unitec.edu>
2018-03-10 23:01:34 -0600

    Third commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,17 @@
 class ProductList extends React.Component {
 	render() {
-		const product = Seed.products[0];
-		return (
-			<div className='ui unstackable items'>
-				<Product
-					id={product.id}
-					title={product.title}
-					description={product.description}
-					url={product.url}
-					votes={product.votes}
-					submitterAvatarUrl={product.submitterAvatarUrl}
-					productImageUrl={product.productImageUrl}
-				/>
-			</div>
-		);
-	}
-}
+		const productComponents = Seed.products.map((product) => (
+		<Product
+			key={'product-' + product.id}
+			id={product.id}
+			title={product.title}
+			description={product.description}
+			url={product.url}
+			votes={product.votes}
+			submitterAvatarUrl={product.submitterAvatarUrl}
+			productImageUrl={product.productImageUrl}
+		/>
+));
 
 ReactDOM.render(
   <ProductList />,
