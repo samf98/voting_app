b27d66a94ade379ff32c8d589d71f19e09780026
samf98 <samir.facusse@unitec.edu>
2018-03-11 00:58:44 -0600

    Final commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,28 @@
 class ProductList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			products: [],
-		};
-	}
+	
+	state = {
+		products: [],
+	};
+
 	componentDidMount() {
 		this.setState({ products: Seed.products });
 	}
-	handleProductUpVote(productId) {
-		console.log(productId + ' was upvoted.');
+
+	handleProductUpVote = (productId) => {
+		const nextProducts = this.state.products.map((product) => {
+			if (product.id === productId) {
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
 	}
+
 	render() {
 		const products = this.state.products.sort((a, b) => (
 			b.votes - a.votes
@@ -28,6 +40,7 @@ class ProductList extends React.Component {
 				onVote={this.handleProductUpVote}
 			/>
 		));
+
 		return (
 			<div className='ui unstackable items'>
 				{productComponents}
@@ -37,13 +50,11 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleUpVote = this.handleUpVote.bind(this);
-	}
-	handleUpVote() {
-		this.props.onVote(this.props.id);
-	}
+
+	handleUpVote = () => (
+		this.props.onVote(this.props.id)
+	);
+
 	render() {
 		return (
 			<div className='item'>
